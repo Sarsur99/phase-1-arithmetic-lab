909e5d491898c5d6f8aac97f2642e170cbe80e7f
Soren <sorenisme@gmail.com>
2022-09-19 17:44:09 -0400

    commit tree b17ff979fef95b6cafacd1ad1858a34f37377406

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 4
+const num4 = 30
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 1000000) + 1;
+
+let mod = num3 % num4
+
+let max = 20
